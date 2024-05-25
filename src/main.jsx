587a13a9f6ec54397c237bf2fad5d5773314d28a
rohitjainfsl <rohit@fullstacklearning.com>
2024-05-25 17:41:09 +0530

    password generator new code

---
+++ src/main.jsx
@@ -0,0 +1,10 @@
+/* eslint-disable no-unused-vars */
+import React from "react";
+import ReactDOM from "react-dom/client";
+// import MyFile from "./MyFile";
+// import Sum from "./Sum";
+// import Counter from "./Counter";
+// import TodoList from "./TodoList";
+import Main from "./passwordGenerator/Main";
+
+ReactDOM.createRoot(document.getElementById("root")).render(<Main />);
